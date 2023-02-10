7ec763df6928c9a1701da6a693a265cf94a2e585
Mohammad Omar <mohammadomartaha17@gmail.com>
2023-02-10 15:24:44 +0200

    chores: clean up project files and add eslint and prettier

---
+++ src/contentScript/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ContentScript from './contentScript';
+
+function init() {
+  const timer = setInterval(() => {
+    const targetDiv = document.getElementsByClassName('mt-3 flex space-x-4')[0];
+    if (targetDiv) {
+      clearInterval(timer);
+
+      const appContainer = document.createElement('div');
+      targetDiv.appendChild(appContainer);
+      if (!appContainer) {
+        throw new Error('Can not find AppContainer');
+      }
+      const root = createRoot(appContainer);
+      console.log(appContainer);
+      root.render(<ContentScript />);
+    }
+  });
+}
+
+init();
